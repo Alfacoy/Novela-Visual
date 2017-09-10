6a8844d6222a24b0f001b835c2534e9745ee69f2
alfacoy <ninanbudevelop@gmail.com>
2017-09-10 19:43:43 -0300

    Init

---
+++ asset/js/main.js
@@ -0,0 +1,11 @@
+//Controladores de Imagenes..
+const background = document.querySelector('.flex-cont').style.backgroundImage ="url(asset/image/background/habitacion1.jpg)"
+
+const character = document.querySelector('#char').src = "asset/image/actor/mama1.png"
+
+const heart = document.querySelector('#love').src ="asset/image/varios/corazon.png"
+
+
+//Controlador de Consola
+
+const console = document.querySelector('#consola')
